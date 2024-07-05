2a46852d0c00d20439098c50f5e8e4c339a3204a
parth1609 <parthgajananpatil@gmail.com>
2024-07-06 02:15:13 +0530

    fix the editing issue

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from post import LinkedList
 from datetime import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "your_secret_key_here"  # Add this line for flash messages
 db = SQLAlchemy(app)
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
-    content = db.Column(db.String(2000), nullable=False)
+    content = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"BlogPost {self.title}"
-
-blog_posts = LinkedList()
 
 @app.route('/')
 def home():
     posts = BlogPost.query.order_by(BlogPost.date.desc()).all()
-    blog_posts = LinkedList()  # Reset the LinkedList
-    for post in posts:
-        blog_posts.insert(str(post.id), post.title, post.content, post.date)
-    return render_template('blog.html', posts=blog_posts.traverse())
+    return render_template('blog.html', posts=posts)
+
+@app.route('/post/<int:id>')
+def view_post(id):
+    post = BlogPost.query.get_or_404(id)
+    return render_template('post.html', post=post)
 
 @app.route('/insert', methods=["GET", "POST"])
 def create_post():
@@ -33,10 +33,14 @@ def create_post():
         title = request.form['title']
         content = request.form['content']
         new_post = BlogPost(title=title, content=content)
-        db.session.add(new_post)
-        db.session.commit()
-        blog_posts.insert(str(new_post.id), title, content, new_post.date)
-        return redirect(url_for('home'))
+        try:
+            db.session.add(new_post)
+            db.session.commit()
+            flash('Your post has been created!', 'success')
+            return redirect(url_for('home'))
+        except:
+            db.session.rollback()
+            flash('An error occurred. Please try again.', 'danger')
     return render_template('insert.html')
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
@@ -46,8 +50,8 @@ def edit_post(id):
         post.title = request.form['title']
         post.content = request.form['content']
         db.session.commit()
-        blog_posts.update(str(id), post.title, post.content, post.date)
-        return redirect(url_for('home'))
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('view_post', id=post.id))
     return render_template('edit.html', post=post)
 
 @app.route('/delete/<int:id>')
@@ -55,13 +59,8 @@ def delete_post(id):
     post = BlogPost.query.get_or_404(id)
     db.session.delete(post)
     db.session.commit()
-    blog_posts.delete(str(id))
+    flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
-
-@app.route('/post/<int:id>')
-def view_post(id):
-    post = BlogPost.query.get_or_404(id)
-    return render_template('post.html', post=post)
 
 if __name__ == "__main__":
     with app.app_context():
